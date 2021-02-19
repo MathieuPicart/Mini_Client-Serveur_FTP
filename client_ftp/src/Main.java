9dd2f7991d28e0dab69e2d095ce3f51300247f29
jules <jules.frere35@gmail.com>
2021-02-19 09:19:51 +0100

    exemple communication

--- client_ftp/src/Main.java
+++ client_ftp/src/Main.java
@@ -1,5 +1,35 @@
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("DAB");
+
+        String hostname = "localhost";
+        int port = 2121;
+
+        try (Socket socket = new Socket(hostname, port)) {
+
+            InputStream input = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+            String bienvenue = reader.readLine();
+
+            System.out.println(bienvenue);
+
+            String msg = reader.readLine();
+
+            System.out.println(msg);
+
+            PrintStream ps = new PrintStream(socket.getOutputStream());
+            ps.println("USER personne");
+
+        } catch (UnknownHostException e) {
+            System.out.println(e);
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println(e);
+            e.printStackTrace();
+        }
     }
 }
