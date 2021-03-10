a60723715e2629f8b0bf738fc1cc14290c0cb803
jules <jules.frere35@gmail.com>
2021-03-10 14:37:14 +0100

    serveur switch

--- serveur_ftp/CommandExecutor.java
+++ serveur_ftp/CommandExecutor.java
@@ -7,34 +7,40 @@ public class CommandExecutor {
 	
 	public static void executeCommande(PrintStream ps, String commande) {
 		if(userOk && pwOk) {
-
-			// Changer de repertoire. Un (..) permet de revenir au repertoire superieur
-			if(commande.split(" ")[0].equals("cd")) (new CommandeCD(ps, commande)).execute();
-	
-			// Telecharger un fichier
-			if(commande.split(" ")[0].equals("get")) (new CommandeGET(ps, commande)).execute();
-			
-			// Afficher la liste des fichiers et des dossiers du repertoire courant
-			if(commande.split(" ")[0].equals("ls")) (new CommandeLS(ps, commande)).execute();
-		
-			// Afficher le repertoire courant
-			if(commande.split(" ")[0].equals("pwd")) (new CommandePWD(ps, commande)).execute();
-			
-			// Envoyer (uploader) un fichier
-			if(commande.split(" ")[0].equals("stor")) (new CommandeSTOR(ps, commande)).execute();
-
-			ps.println("2 La commande n'existe pas");
-							}
-		else {
-			if(commande.split(" ")[0].equals("pass") || commande.split(" ")[0].equals("user")) {
-				// Le mot de passe pour l'authentification
-				if(commande.split(" ")[0].equals("pass")) (new CommandePASS(ps, commande)).execute();
-	
-				// Le login pour l'authentification
-				if(commande.split(" ")[0].equals("user")) (new CommandeUSER(ps, commande)).execute();
+			switch (commande.split(" ")[0]) {
+				case "cd" :
+					(new CommandeCD(ps, commande)).execute();
+					break;
+				case "get" :
+					(new CommandeGET(ps, commande)).execute();
+					break;
+				case "ls" :
+					(new CommandeLS(ps, commande)).execute();
+					break;
+				case "pwd" :
+					(new CommandePWD(ps, commande)).execute();
+					break;
+				case "stor" :
+					(new CommandeSTOR(ps, commande)).execute();
+					break;
+				default:
+					ps.println("2 La commande n'existe pas");
+					break;
 			}
-			else
-				ps.println("2 Vous n'êtes pas connecté !");
+
+		} else {
+			switch (commande.split(" ")[0]) {
+				case "pass":
+					(new CommandePASS(ps, commande)).execute();
+					break;
+				case "user":
+					(new CommandeUSER(ps, commande)).execute();
+					break;
+				default:
+					ps.println("2 La commande n'existe pas");
+					break;
+			}
+
 		}
 	}
 
