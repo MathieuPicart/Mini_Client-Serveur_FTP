d32681d899327109998eff921d8fe5a85cb69e4a
MathieuPicart <mat.pic@outlook.fr>
2021-03-11 17:17:56 +0100

    CD fait !

--- serveur_ftp/CommandeCD.java
+++ serveur_ftp/CommandeCD.java
@@ -8,13 +8,12 @@ public class CommandeCD extends Commande {
 
 	public void execute() {
 		File dossier = new File(CommandExecutor.racinePath + commandeArgs[0]);
-		System.out.println(CommandExecutor.racinePath);
-		System.out.println("Path file : " + dossier.getAbsolutePath().toString());
 		if (dossier.exists() && dossier.isDirectory()) {
-			if (dossier.getPath().contains(CommandExecutor.racinePath)) {
+			if (dossier.toPath().normalize().toString().replace("\\", "/").contains(CommandExecutor.racinePath)) {
+				CommandExecutor.currentPath = dossier.toPath().normalize().toString().replace("\\", "/").replace(CommandExecutor.racinePath,"");
 				ps.println("0 Commande cd OK");
 			} else {
-				ps.println("2 Vous n'avez pas acces au dossier " + commandeArgs[0]);
+				ps.println("2 Vous n'avez pas acces au dossier");
 			}
 		} else {
 			ps.println("2 Le dossier " + commandeArgs[0] + " n'existe pas");
